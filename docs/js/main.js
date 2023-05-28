1ac132ed6971827219d3607c4535e0d1576702cc
WasilyR <127860653+WasilyR@users.noreply.github.com>
2023-05-28 17:19:47 +0300

    Add files via upload

---
+++ docs/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+		$('.portfolio__slider').slick({
+			arrows: true,
+			dots: false,
+			Infinite: true,
+			slidesToShow: 1,
+			waitForAnimate: false,
+			centerMode: false,
+
+		});
+		
+		});
+
+
+
+
+
+
